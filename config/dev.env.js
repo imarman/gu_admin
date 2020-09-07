6ebc41427f6566d708c9df34b4352a3d7f6a5ce6
Arman <arman503@163.com>
2020-09-07 15:46:54 +0800

    后台管理界面

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  // 后端接口
+  BASE_API: '"http://localhost:8110"',
+})
