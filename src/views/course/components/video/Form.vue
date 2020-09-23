7918dcbd860319982464cc823b4afae920f3c37a
Arman <arman503@163.com>
2020-09-23 23:23:25 +0800

    vod 视频上传删除

--- src/views/course/components/video/Form.vue
+++ src/views/course/components/video/Form.vue
@@ -14,6 +14,28 @@
           <el-radio :label="false">默认</el-radio>
         </el-radio-group>
       </el-form-item>
+      <!-- 上传视频 -->
+      <el-form-item label="上传视频">
+        <el-upload
+          ref="upload"
+          :auto-upload="false"
+          :on-success="handleUploadSuccess"
+          :on-error="handleUploadError"
+          :on-exceed="handleUploadExceed"
+          :before-remove="handleBeforeRemove"
+          :on-remove="handleOnRemove"
+          :file-list="fileList"
+          :limit="1"
+          action="http://127.0.0.1:9130/admin/vod/media/upload">
+          <el-button slot="trigger" size="small" type="primary">选择视频</el-button>
+          <el-button
+            :disabled="uploadBtnDisabled"
+            style="margin-left: 10px;"
+            size="small"
+            type="success"
+            @click="submitUpload()">上传</el-button>
+        </el-upload>
+      </el-form-item>
     </el-form>
     <div slot="footer" class="dialog-footer">
       <el-button @click="close()">取 消</el-button>
@@ -24,7 +46,7 @@
 
 <script>
 import videoApi from '@/api/video'
-
+import vodApi from '@/api/vod'
 export default {
   data() {
     return {
@@ -32,10 +54,56 @@ export default {
       video: {
         sort: 0,
         free: false
-      }
+      },
+      // 上传文件列表
+      fileList: [],
+      uploadBtnDisabled: false
     }
   },
   methods: {
+    // 删除视频文件确认
+    handleBeforeRemove(file, fileList) {
+      return this.$confirm(`确定移除 ${file.name}？`)
+    },
+    // 执行视频文件的删除
+    handleOnRemove(file, fileList) {
+      if (!this.video.videoSourceId) {
+        return
+      }
+      vodApi.removeByVodId(this.video.videoSourceId).then(response => {
+        this.video.videoSourceId = ''
+        this.video.videoOriginalName = ''
+        // 如果没有这一步, 那么点击取消之后 vod 视频会被删除, 但是数据库却没有删除资源
+        // 删除的同时更新 video 数据信息
+        videoApi.updateById(this.video)
+        this.$message.success(response.message)
+      })
+    },
+    // 上传
+    submitUpload() {
+      this.uploadBtnDisabled = true
+      this.$refs.upload.submit() // 提交上传请求
+    },
+    // 视频上传成功的回调
+    handleUploadSuccess(response, file, fileList) {
+      this.uploadBtnDisabled = false
+      if (response.success) {
+        // 给 video_source_id 赋值
+        this.video.videoSourceId = response.data.videoId
+        this.video.videoOriginalName = file.name
+      } else {
+        this.$message.error('上传失败(非20000)')
+      }
+    },
+    // 上传失败回调
+    handleUploadError() {
+      this.uploadBtnDisabled = false
+      this.$message.error('上传失败(http)')
+    },
+    // 上传多于一个视频
+    handleUploadExceed(files, fileList) {
+      this.$message.warning('想要重新上传视频，请先删除已上传的视频')
+    },
     // 保存或更新
     saveOrUpdate() {
       if (!this.video.id) {
@@ -68,10 +136,13 @@ export default {
     // 关闭对话框
     close() {
       this.dialogVisible = false
+      // 清空 video
       this.video = {
         sort: 0,
         free: false
       }
+      // 清空 fileList
+      this.fileList = []
     },
     // 开启对话框
     open(chapterId, videoId) {
@@ -80,6 +151,11 @@ export default {
       if (videoId) {
         videoApi.getById(videoId).then(resp => {
           this.video = resp.data.item
+          // 回显
+          // debugger
+          if (this.video.videoSourceId) {
+            this.fileList = [{ 'name': this.video.videoOriginalName }]
+          }
         })
       }
     }
